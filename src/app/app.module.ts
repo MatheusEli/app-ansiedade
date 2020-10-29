1e7b265ac026ac41f110d39638909f0fbf9cddee
MatheusEli <matheuselisouzaferreira@hotmail.com>
2020-10-28 22:48:18 -0300

    importando firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), 
+    AppRoutingModule,AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyClTgtFEy3fZki_kUIJJUn3dpktgYFWia0",
+      authDomain: "app-ansiedade.firebaseapp.com",
+      databaseURL: "https://app-ansiedade.firebaseio.com",
+      projectId: "app-ansiedade",
+      storageBucket: "app-ansiedade.appspot.com",
+      messagingSenderId: "51638047326"
+  }
 };
 
 /*
