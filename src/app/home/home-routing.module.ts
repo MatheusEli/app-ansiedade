329255dd56e481d31ad71f4fcc8f05ca218b46f9
MatheusEli <matheuselisouzaferreira@hotmail.com>
2020-11-08 16:21:21 -0300

    alterando sobre

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -24,11 +24,6 @@ const routes: Routes = [
         path: 'perfil',
         loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
       },
-      {
-        path: 'musicoterapia',
-        loadChildren: () => import('./musicoterapia/musicoterapia.module').then( m => m.MusicoterapiaPageModule)
-      },
-
     ]
   },
 
