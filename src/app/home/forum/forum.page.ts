7bc73d902bd2aa4478f620885ae0f1c86c970c84
MatheusEli <matheuselisouzaferreira@hotmail.com>
2020-11-08 22:10:39 -0300

    organizando pagina

--- src/app/home/forum/forum.page.ts
+++ src/app/home/forum/forum.page.ts
@@ -1,4 +1,3 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
