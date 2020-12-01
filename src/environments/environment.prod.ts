992522cc253d097ea0448e5040c38bbeeb5f7ac4
MatheusEli <matheuselisouzaferreira@hotmail.com>
2020-12-01 20:32:10 -0300

    configurando netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "...",
-    authDomain: "project.firebaseapp.com",
-    databaseURL: "https://project.firebaseio.com/",
-    projectId: "project",
-    storageBucket: "project.appspot.com",
-    messagingSenderId: "..."
-  }
+  production: true
 };
