ff5bd1d0ae245181fbff37dd828ee1d79c9e1693
MatheusEli <matheuselisouzaferreira@hotmail.com>
2020-12-01 20:34:25 -0300

    configurando enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyClTgtFEy3fZki_kUIJJUn3dpktgYFWia0",
+      authDomain: "app-ansiedade.firebaseapp.com",
+      databaseURL: "https://app-ansiedade.firebaseio.com",
+      projectId: "app-ansiedade",
+      storageBucket: "app-ansiedade.appspot.com",
+      messagingSenderId: "51638047326"
+  }
 };
